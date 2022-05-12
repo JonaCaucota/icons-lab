ab0e05ef91b39ec27bb90337f1782039f96359b6
JonaCaucota <jonacau12@gmail.com>
2022-05-12 11:12:09 -0300

    Desafio primera semana

---
+++ src/main/java/controller/ContinenteController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("continentes")
+public class ContinenteController {
+
+	public x save(@RequestBody ContinenteDTO continente) {
+		
+	}
+}
